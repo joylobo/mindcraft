0443120878582f76b499f818991326410e7ff243
Joy Lobo <joylobo0528@gmail.com>
2024-11-05 18:05:45 +0800

    改用百度翻译

--- src/utils/translator.js
+++ src/utils/translator.js
@@ -1,7 +1,20 @@
-import translate from 'google-translate-api-x';
+import _ from 'lodash';
+import googleTranslate from 'google-translate-api-x';
+import BaiduTranslate from 'baidu-translate';
 import settings from '../../settings.js';
 
+const translator = String(settings.translator).toLowerCase();
 const preferred_lang = String(settings.language).toLowerCase();
+
+async function translate(message, opts) {
+    if (translator === 'baidu') {
+        const baiduTranslate = BaiduTranslate(settings.baiduTranslate.appid, settings.baiduTranslate.secret, opts.to);
+        const trans_result = await baiduTranslate(message);
+        return { text: _.get(trans_result, 'trans_result[0].dst', message) };
+    }
+
+    return await googleTranslate(message, opts);
+}
 
 export async function handleTranslation(message) {
     if (preferred_lang === 'en' || preferred_lang === 'english')
